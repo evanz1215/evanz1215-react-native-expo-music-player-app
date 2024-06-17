2fffebe53512882dee36b85bce14fc855a4190de
Evan <evanz1215@gmail.com>
2024-06-17 23:46:35 +0800

    chore: Update gitignore and tsconfig.json
    The gitignore file was updated to include the generated expo-cli patterns. The tsconfig.json file was modified to add additional compiler options and paths for module resolution.

---
+++ src/app/(tabs)/(songs)/index.tsx
@@ -0,0 +1,12 @@
+import { defaultStyles } from '@/style'
+import { View, Text } from 'react-native'
+
+const SongsScreen = () => {
+	return (
+		<View style={defaultStyles.container}>
+			<Text style={defaultStyles.text}>Songs screen</Text>
+		</View>
+	)
+}
+
+export default SongsScreen
